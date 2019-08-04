860e87ce57c9ce11eab9f480102af6066f747b8c
Dipen Desai <dnd.devops@gmail.com>
2019-08-04 04:53:42 -0700

    Fixing ListDir Python command

--- PY_TEST/OS/OS_Info.py
+++ PY_TEST/OS/OS_Info.py
@@ -1,31 +1,8 @@
 import os as os
 
-print(f'PROCESS ID (PID): {os.getpid()}')
-print(f'PWD: {os.getcwd()}')
-print(f'LIST DIR: {os.listdir()}')
+print('PROCESS ID (PID): {0}'.format(os.getpid()))
+print('PWD: {0}'.format(os.getcwd()))
+print('LIST DIR: {0}'.format(os.listdir(os.getcwd())))
 
-os.chdir('C:\\Users\\RRDD\\Desktop')
-
-os.makedirs('DDDDD')
-
-
-
-
-
-
-# 
-# a = 700
-# try:
-#     #a = int(input("Enter Number ONLY: "))
-#     f = lambda b: "G" if b>100 else "S"
-#     print(f(a))
-# except:
-#     print("Exception")
-# 
-# print("User Input: ", a)
-# 
-# 
-# g = lambda b: 2 if b>100 else (1 if (b <=100 and b > 0) else 0)
-# print(g(1000))
-# print(g(20))
-# print(g(0))
+# os.chdir('C:\\Users\\RRDD\\Desktop')
+# os.makedirs('DDDDD')
