300b328eb8fccebfcfe9c87536d7c202ea06b8cf
Dipen Desai <dnd.devops@gmail.com>
2020-03-29 06:50:21 -0700

    Enhancing functionality by adding GREP in bash command

--- PY_TEST/Basic/version.py
+++ PY_TEST/Basic/version.py
@@ -7,6 +7,7 @@
 #   $ python ./version.py web all
 #   $ python ./version.py app svc-heloc-application
 #   $ python ./version.py app all
+#   $ python ./version.py app gds
 
 import os
 import sys
@@ -33,9 +34,17 @@ class version(object):
 
   # K8S VERSIONS *******************************************************************************************************************************************************
   def find_k8s_version(self, context, namespace):
-    # command = "kubectl get pods --context "+context+" -n "+namespace+" -oyaml | grep -e 'image: docker.prosper.com' | sed 's|.*.com\/||g' | uniq"
-    command = "kubectl get pods --context "+context+" -n "+namespace+" -o jsonpath='{.items[*].spec.containers[*].image}' | tr -s '[[:space:]]' '\n' | sed 's/docker.prosper.com\///g' | sort | uniq"
-    # command = "kubectl get pods --context "+context+" -n "+namespace+" -o jsonpath='{.items[*].status.containerStatuses[*].image}' | sed 's/docker.prosper.com\///g' | tr -s '[[:space:]]' '\n'  | sort | uniq"
+    parser = argparse.ArgumentParser()
+    parser.add_argument('app')
+    args = parser.parse_args(sys.argv[2:])
+
+    grep_value = ""
+    if (args.app != "all"):
+      grep_value = " | grep " + args.app
+
+    # command = "kubectl get pods --context "+context+" -n "+namespace+" -oyaml | grep -e 'image: docker.prosper.com' | sed 's|.*.com\/||g' | uniq" + grep_value
+    command = "kubectl get pods --context "+context+" -n "+namespace+" -o jsonpath='{.items[*].spec.containers[*].image}' | tr -s '[[:space:]]' '\n' | sed 's/docker.prosper.com\///g' | sort | uniq" + grep_value
+    # command = "kubectl get pods --context "+context+" -n "+namespace+" -o jsonpath='{.items[*].status.containerStatuses[*].image}' | sed 's/docker.prosper.com\///g' | tr -s '[[:space:]]' '\n'  | sort | uniq" + grep_value
     process = os.popen(command)
 
     helm_Data = file.read(process)
@@ -50,11 +59,11 @@ class version(object):
     return helm_Data.split("\n")
 
   # PARSE VERSIONS *******************************************************************************************************************************************************
-  def assign_Version(self, env, envList):
+  def assign_Version(self, env, appVersions):
     self.version_All["APPLICATION"] = ["QA32", "STAGE", "UAT", "PROD"]
 
-    for i in range(len(envList)-1):
-      temp = envList[i].split(":")
+    for i in range(len(appVersions)-1):
+      temp = appVersions[i].split(":")
 
       if (env == "qa"):
         ee = 0
@@ -74,29 +83,20 @@ class version(object):
 
   # PRINT TABLE *******************************************************************************************************************************************************
   def print_table(self):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('app')
-    args = parser.parse_args(sys.argv[2:])  
-
     keys = sorted(self.version_All.keys())
     max_len = len(max(keys, key=len)) + 1
     tc = 0    # temp_count (looping for version_All directory keys)
 
     for i in range(len(keys) + 3):
-      c = ""
-      
       if (i == 0 or i == 2 or (i - (len(keys) + 2)) == 0):
         c = sep["v"] + sep["h"]*(max_len+2) + sep["v"] + (sep["h"]*44 + sep["v"])*4     # PRINTING HORIZONAL LINES
-      elif (i == 1 or args.app == "all" or args.app == keys[tc]):
+      else:
         c = sep["v"] + "  " + keys[tc] + " "*(max_len - len(keys[tc])) + sep["v"]
         for x in range(0, 4):
           c = c + "  " + self.version_All[keys[tc]][x] + " "*(42-len(self.version_All[keys[tc]][x])) + sep["v"]
         tc += 1
-      else:
-        tc += 1
-        
-      if (len(c) > 0):
-        print(c)
+    
+      print(c)
 
   def web(self):
     self.version_All = {}
