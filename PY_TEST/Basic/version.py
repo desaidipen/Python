c9f7382d9884fcec6db0f22438c0597801714054
Dipen Desai <dnd.devops@gmail.com>
2020-05-13 17:14:53 -0700

    Improvising to supply only one argument

--- PY_TEST/Basic/version.py
+++ PY_TEST/Basic/version.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python2
 
 # Prints application versions across all environments
-# Command: $ python ..../version.py {type} {application}
-# E.g. 
-#   $ python ./version.py web web-graphql
-#   $ python ./version.py web all
-#   $ python ./version.py app svc-heloc-application
-#   $ python ./version.py app all
-#   $ python ./version.py app gds
+# Command: $ python ..../version.py {application}
+#   $ python ./version.py all
+#   $ python ./version.py graphql
+#   $ python ./version.py heloc-application
 
 import os
 import sys
@@ -16,20 +13,15 @@ import argparse
 sep = {"h": "-", "v": "|"}
 
 class version(object):
-  def __init__(self):
-    self.version_All = {}
+  # RESET VERSIONS *******************************************************************************************************************************************************
+  def reset_All(self):
+    self.version_All = {"APPLICATION": []}
     self.version_Length = []
-    parser = argparse.ArgumentParser()
-    parser.add_argument("type")
-    args=parser.parse_args(sys.argv[1:2])
 
-    if (args.type == 'all'):
-      print ("K8S-WEB")
-      self.web()
-      print ("\nK8S-APP")
-      self.app()
-    else:
-      getattr(self, args.type)()
+  def __init__(self):
+    self.reset_All
+    self.web()
+    self.app()
 
   # K8S VERSIONS *******************************************************************************************************************************************************
   def find_k8s_version(self, context, namespace):
@@ -38,7 +30,7 @@ class version(object):
 
     parser = argparse.ArgumentParser()
     parser.add_argument('app')
-    args = parser.parse_args(sys.argv[2:])
+    args = parser.parse_args(sys.argv[1:])
 
     grep_value = ""
     if (args.app != "all"):
@@ -55,7 +47,6 @@ class version(object):
   # PARSE VERSIONS *******************************************************************************************************************************************************
   def assign_Version(self, appVersions):
     ee = len(self.version_All["APPLICATION"]) - 1
-
     for i in range(len(appVersions)-1):
       temp = appVersions[i].split(":")
       
@@ -71,42 +62,24 @@ class version(object):
   # PRINT TABLE *******************************************************************************************************************************************************
   def print_table(self):
     keys = sorted(self.version_All.keys())
-    max_len = len(max(keys, key=len)) + 1
-    tc = 0    # temp_count (looping for version_All directory keys)
 
-    envCount = len(self.version_All["APPLICATION"])
+    if (len(keys) > 1):
+      max_len = len(max(keys, key=len)) + 1
+      tc = 0    # temp_count (looping for version_All directory keys)
+      envCount = len(self.version_All["APPLICATION"])
 
-    for i in range(len(keys) + 3):
-      if (i == 0 or i == 2 or (i - (len(keys) + 2)) == 0):
-        c = sep["v"] + sep["h"]*(max_len+2) + sep["v"]
-        for x in range(0, envCount):
-          c = c + sep["h"]*(self.version_Length[x]+2) + sep["v"]
-      else:
-        c = sep["v"] + "  " + keys[tc] + " "*(max_len - len(keys[tc])) + sep["v"]
-        for x in range(0, envCount):
-          c = c + "  " + self.version_All[keys[tc]][x] + " "*(self.version_Length[x]-len(self.version_All[keys[tc]][x])) + sep["v"]
-        tc += 1
-    
-      print(c)
-  
-  # RESET VERSIONS *******************************************************************************************************************************************************
-  def reset_All(self):
-    self.version_All = {"APPLICATION": []}
-    self.version_Length = []
-
-  # def function_call(self, envList):
-  #   self.reset_All()
-  #   for key in envList.keys():
-  #     self.assign_Version(self.find_k8s_version(key, envList[key]))
-  #   self.print_table()
-
-  # def web(self):
-  #   envList = {"web-qa": "qa32", "np": "stage", "web-uat": "uat", "web-prod": "prod"}
-  #   self.function_call(envList)
-
-  # def app(self):
-  #   envList = {"app-qa": "qa32", "app-stage": "stage", "app-uat": "uat", "app-prod": "prod"}
-  #   self.function_call(envList)
+      for i in range(len(keys) + 3):
+        if (i == 0 or i == 2 or (i - (len(keys) + 2)) == 0):
+          c = sep["v"] + sep["h"]*(max_len+2) + sep["v"]
+          for x in range(0, envCount):
+            c = c + sep["h"]*(self.version_Length[x]+2) + sep["v"]
+        else:
+          c = sep["v"] + "  " + keys[tc] + " "*(max_len - len(keys[tc])) + sep["v"]
+          for x in range(0, envCount):
+            c = c + "  " + self.version_All[keys[tc]][x] + " "*(self.version_Length[x]-len(self.version_All[keys[tc]][x])) + sep["v"]
+          tc += 1
+      
+        print(c)
 
   def web(self):
     self.reset_All()
