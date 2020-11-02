2b7d65d0256df608a378039cbd883be8c5ebcdd3
Desai <desadipe@3c22fb7b16eb.ant.amazon.com>
2020-11-02 13:24:52 -0600

    Adding test.py in OS + Adding AWS SQS script

---
+++ PY_TEST/OS/os_aws_sqs.py
@@ -0,0 +1,10 @@
+import os
+import random
+import time
+
+for i in range (40):
+    rl = random.randint(1, 25)
+    shell_command = "aws sqs send-message --region us-east-1 --queue-url https://sqs.us-east-1.amazonaws.com/781641733284/dd-one-queue --message-body {}".format(rl)
+    msg_id = os.popen(shell_command)
+    print('Random No: {0} --- Msg ID: {1}'.format(shell_command, msg_id.read()))
+    time.sleep(random.randint(1, 15))
--- PY_TEST/OS/test.py
+++
@@ -1,8 +0,0 @@
-import os
-import random
-
-for i in range (10):
-    rl = random.randint(1, 25)
-    shell_command = "aws sqs send-message --region us-east-1 --queue-url https://sqs.us-east-1.amazonaws.com/781641733284/dd-one-queue --message-body {}".format(rl)
-    msg_id = os.popen(shell_command)
-    print('Random No: {0} --- Msg ID: {1}'.format(shell_command, msg_id.read()))
