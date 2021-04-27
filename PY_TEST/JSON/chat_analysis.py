c829a6954615aebe58008c24ce28f46f779d7ca3
Desai <desadipe@3c22fb7b16eb.ant.amazon.com>
2021-04-27 15:54:15 -0500

    Updating evalgui and chat-analysis

--- PY_TEST/JSON/chat_analysis.py
+++ PY_TEST/JSON/chat_analysis.py
@@ -1,47 +1,69 @@
+import sys
 from datetime import datetime
 
-names = {}
 times = {}
-mL = 9
+names = {}
+modules = {}
+n_length = 9
+m_length = 11
+
 s_chat = 0
 
-with open('/Users/desadipe/Documents/python/Python/PY_TEST/JSON/chat_data.txt', 'r') as fr:
+with open(sys.argv[1], 'r') as fr:
     line = fr.readline()
+    line = line.replace("PM", "PM ") if ("PM" in line) else line.replace("AM", "AM ")
+    module = "General"
+    
     while len(line) > 0:
         try:
-            t = line.split(":")[0] + ":00" + line.split(" ")[1]
-            t = datetime.strptime(t, "%I:%M%p").strftime("%H:%M")
-            times[t] = times.get(t, 0) + 1
-
-            if (("AM" in line) or ("PM" in line)):
+            if ((("AM" in line) or ("PM" in line)) and ("MODULE" not in line)):
                 splt = "PM " if ("PM " in line) else "AM "
                 temp = line.split(splt) 
                 if (temp[1].split(" ")[0] != "To"):
+                    t = line.split(":")[0] + ":00" + line.split(" ")[1]
+                    t = datetime.strptime(t, "%I:%M%p").strftime("%H:%M")
+                    times[t] = times.get(t, 0) + 1
+
                     n = temp[1].split("(")[0].rstrip()
-                    mL = len(n) if len(n) > mL else mL
+                    n_length = len(n) if len(n) > n_length else n_length
                     names[n] = names.get(n, 0) + 1
+
+                    modules[module] = modules.get(module, 0) + 1
+
                     s_chat = s_chat + 1
                 # else:
                 #     n = "Organizer"
                 #     names[n] = names.get(n, 0) + 1
-
+            elif ('>>>>> MODULE' in line):
+                module = line.replace(">>>>> ", "").rstrip()
+                m_length = len(module) if len(module) > m_length else m_length
         except:
             None
         line = fr.readline()
+        line = line.replace("PM", "PM ") if ("PM" in line) else line.replace("AM", "AM ")
 
 t_sorted = sorted(times.keys())
-print ("|---------|----------|")
-print ("|  TIME   |  EVENTS  |")
-print ("|---------|----------|")
+print ("|---------|---------|")
+print ("|  TIME   |  CHATS  |")
+print ("|---------|---------|")
 for t in t_sorted:
-    print ("|{:>7}  |{:>8}  |".format(t, times[t]))
-print ("|---------|----------|\n")
+    print ("|{:>7}  |{:>7}  |".format(t, times[t]))
+print ("|---------|---------|\n")
 
 n_sorted = sorted(names.keys())
-print ("|{}|----------|".format("-"*(mL+4)))
-print ("|  NAMES  {}|  EVENTS  |".format(" "*(mL-5)))
-print ("|{}|----------|".format("-"*(mL+4)))
+print ("|{}|---------|".format("-"*(n_length+4)))
+print ("|  NAMES  {}|  CHATS  |".format(" "*(n_length-5)))
+print ("|{}|---------|".format("-"*(n_length+4)))
 for n in n_sorted:
-    print ("|  {:<{}}|{:>8}  |".format(n, mL+2, names[n]))
-print ("|{}|----------|".format("-"*(mL+4)))
+    print ("|  {:<{}}|{:>7}  |".format(n, n_length+2, names[n]))
+print ("|{}|---------|\n".format("-"*(n_length+4)))
+
+m_sorted = sorted(modules.keys())
+print ("|{}|---------|".format("-"*(m_length+4)))
+print ("|  MODULES  {}|  CHATS  |".format(" "*(m_length-7)))
+print ("|{}|---------|".format("-"*(m_length+4)))
+for m in m_sorted:
+    print ("|  {:<{}}|{:>7}  |".format(m, m_length+2, modules[m]))
+print ("|{}|---------|\n".format("-"*(m_length+4)))
+
 print ("Total Student Chat Count: {}".format(s_chat))
